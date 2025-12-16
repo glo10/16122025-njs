d035851262b6aeae68aec1fee0b14652def28824
glodie <glodie.tshimini@gmail.com>
2025-12-16 15:53:52 +0100

    refactor: add readFile

--- demos/7-fs/index.mjs
+++ demos/7-fs/index.mjs
@@ -6,10 +6,22 @@ const me = {
     lastname: 'Tshimini',
     age: 34
 }
+// écriture
 writeFile('./me.txt',
     JSON.stringify(me),
     { encoding: 'utf-8'},
     (err) => {
     if(err) console.error('KO')
     console.info('OK')
+})
+
+// Lecture
+readFile('./me.txt', (err, content) => {
+    if(!err)  {
+        const meAsObject = JSON.parse(content)
+        const meAsString = content.toString()
+        console.log('string', meAsString, 'object', meAsObject)
+        console.log(`My name is ${meAsObject.firstname}`)
+    }
+    else console.error('error', err)
 })

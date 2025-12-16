1450df5b87e6113459cf9570ed1719af8121059a
glodie <glodie.tshimini@gmail.com>
2025-12-16 12:41:21 +0100

    refactor: use as to rename

---
+++ demos/2-ecmascript/functions/math.mjs
@@ -0,0 +1,13 @@
+export default class MyMath {
+  constructor() {
+    console.log("constructor");
+  }
+
+  sum() {
+    console.log("sum");
+  }
+
+  multiply() {
+    console.log("multiply");
+  }
+}
--- demos/2-ecmascript/index.mjs
+++ demos/2-ecmascript/index.mjs
@@ -1,5 +1,8 @@
-import {lang, capitalize, firstLetterCapitalize } from './functions/string.mjs'
-
-console.log('La langue est ', lang)
+import {lang as renameLang, capitalize, firstLetterCapitalize } from './functions/string.mjs'
+import Toto from './functions/math.mjs'
+console.log('La langue est stockée la variable lang qui a été renommée', renameLang)
 capitalize()
 firstLetterCapitalize()
+const newToto = new Toto()
+newToto.sum()
+newToto.multiply()

baebb82da28472fa2eec586170019b19524c9502
glodie <glodie.tshimini@gmail.com>
2025-12-16 12:06:04 +0100

    feat: demo npm and ESM

---
+++ demos/2-ecmascript/functions/string.mjs
@@ -0,0 +1,9 @@
+export const lang = 'FR'
+
+export function capitalize() {
+    console.log('capitalize')
+}
+
+export function firstLetterCapitalize() {
+    console.log('firstLetterCapitalize')
+}
---
+++ demos/2-ecmascript/index.mjs
@@ -0,0 +1,5 @@
+import {lang, capitalize, firstLetterCapitalize } from './functions/string.mjs'
+
+console.log('La langue est ', lang)
+capitalize()
+firstLetterCapitalize()

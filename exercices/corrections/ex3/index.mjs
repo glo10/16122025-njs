e8144f724ca12a5f8ae10e394ccd387be532524e
glodie <glodie.tshimini@gmail.com>
2025-12-17 11:40:04 +0100

    refactor: update index.mjs ex3

--- exercices/corrections/ex3/index.mjs
+++ exercices/corrections/ex3/index.mjs
@@ -1,4 +1,5 @@
 import { mkdir, writeFile } from "node:fs";
+import { access } from "node:fs/promises";
 import { createInterface } from "node:readline/promises";
 
 const app = createInterface({
@@ -13,15 +14,17 @@ const data = JSON.stringify({
   firstName,
   lastName,
 })
-const dirOrFileName = `${firstName}_${lastName}`
+const directory = `users`
+// Générer un nom de fichier aléatoire pour respecter RGPD
+const filename = Math.ceil(Math.random() *10000)
 // Création du fichier JSON
-writeFile(`${dirOrFileName}.json`, data, (err) =>  {
-  if(!err) console.log('JSON OK')
-  else console.error('JSON KO', err)
+writeFile(`${directory}/${filename}.json`, data, (err) =>  {
+  if(!err) console.log('Write OK')
+  else console.error('Write KO', err)
 })
 
-// Création du dossier, idéalement ici pour
-mkdir(dirOrFileName, (err) => {
+// Création du dossier, idéalement faire la gestion du dossier qui existe déjà
+mkdir(directory, (err) => {
   if(!err) console.log('Dir OK')
   else console.error('Dir KO', err)
 })

f4e4e40181dbf4fa481afa73eb527fce89bc9a2f
glodie <glodie.tshimini@gmail.com>
2025-12-19 13:43:23 +0100

    feat: connect to mongoDB

---
+++ exercices/corrections/ex10-11-live/models/connect.js
@@ -0,0 +1,22 @@
+import { mongoose } from 'mongoose'
+export function connect() {
+    // Propriété readyState nous donne l'info sur l'état de connexion à bdd
+    const state = mongoose.connection.readyState
+    /**
+     * les valeurs possibles de state
+     *  0 => disconnected
+     *  1 => connected
+     *  2 => connecting (en cours de connexion)
+     *  3 => disconnecting (en cours de déconnexion)
+     */
+    if(state != 1) {
+        const url = process.env.ME_CONFIG_MONGODB_URL
+        mongoose.connect(url)
+        .then(() => {
+            console.log('connexion OK', mongoose.connection.readyState) // égale à 1
+        }).catch(err => {
+            console.error('connexion KO', err)
+        })
+    }
+
+}
